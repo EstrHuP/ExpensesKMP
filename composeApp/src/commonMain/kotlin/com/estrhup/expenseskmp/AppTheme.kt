2f465496cf96ccb8ab9d59f2c097b2267f559ebd
EstrHuP <estrhup@gmail.com>
2025-03-25 16:57:31 +0100

    first commit

---
+++ composeApp/src/commonMain/kotlin/com/estrhup/expenseskmp/AppTheme.kt
@@ -0,0 +1,27 @@
+package com.estrhup.expenseskmp
+
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun getColorsTheme() {
+    val isDarkMode = false
+    val Purple = Color(0xFF6A66FF)
+    val ColorExpenseItem = if (isDarkMode) Color(0xFF090808) else Color(0xFFF1F1F1)
+}
---
+++ composeApp/src/iosMain/kotlin/com/estrhup/expenseskmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.estrhup.expenseskmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
